a4b0e621f8400bfa7d53d2863f4a8d3187238452
Mohammad Sahil <157118513+sahil2431@users.noreply.github.com>
2025-02-23 21:19:00 +0530

    Update firebaseService.js

--- Backend/src/services/firebaseService.js
+++ Backend/src/services/firebaseService.js
@@ -4,10 +4,10 @@ import { Buffer } from "buffer";
 
 const firebaseAdminSdkBase64 = process.env.FIREBASE_ADMIN_SDK;
 const firebaseAdminSdkJson = Buffer.from(firebaseAdminSdkBase64, 'base64').toString('utf-8');
-fs.writeFileSync('./firebase-admin-sdk.json', firebaseAdminSdkJson);
+fs.writeFileSync('/tmp/firebase-admin-sdk.json', firebaseAdminSdkJson);
 
 admin.initializeApp({
-  credential: admin.credential.cert("../firebase-admin-sdk.json"),
+  credential: admin.credential.cert("/tmp/firebase-admin-sdk.json"),
 });
 export const verifyIdToken = async (idToken) => {
   try {
